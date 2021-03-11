bb27c5f2032bee495b6e189d4a23f4b660bd307f
winefinance <winefinanceyield@gmail.com>
2021-03-11 21:57:39 +0300

    init commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,65 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://wineyield.finance/'
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap'
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool'
+      }
+    ]
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://wineyield.finance/farms'
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'https://wineyield.finance/pools'
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://www.goosedefi.com/lottery'
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0x09eb8438451c1C617b6051EED015db988af6Df2A',
+      },
+    ]
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      // {
+      //   label: 'Voting',
+      //   href: 'https://voting.pancakeswap.finance',
+      // },
+      {
+        label: "Github",
+        href: "https://github.com/winefinance/",
+      },
+    ],
+  }
+]
+
+export default config
---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x09eb8438451c1C617b6051EED015db988af6Df2A',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0x975b0A200e212109A7cCFB1aa4A15BF314369036'
+}
+
+export default priceContracts
